4e1c6569c5b4ec9e3b075d9c970ae90a7d8e8c08
Dominic Chaple <d0mch4pl3@gmail.com>
2025-03-31 20:47:31 +0100

    Add stat caching to the authenticated user.

---
+++ src/app/api/stats/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import connect from '@/utils/db';
+import getUser from '@/utils/user';
+
+export const GET = async (request) => {
+  try {
+    const user = await getUser();
+    const stats = {
+      'uniqueAttempts': user.Russian.attemptedCardCount,
+      'uniqueErrors': user.Russian.errorCount,
+      'activeStreak': user.Russian.activeStreak,
+      'longestStreak': user.Russian.longestStreak
+    }
+    return new NextResponse(JSON.stringify(stats), {
+      status: 200,
+    });
+  } catch (err) {
+    return new NextResponse(
+      JSON.stringify({ error: 'Database Error: ' + err }),
+      { status: 500 }
+    );
+  }
+};
--- src/utils/user.js
+++ src/utils/user.js
@@ -1,20 +1,39 @@
 import { getServerSession } from 'next-auth';
 import authOptions from '@/utils/authOptions';
+import mongoose from 'mongoose';
+import connect from '@/utils/db';
+import User from '@/models/User';
 
-const getUser = async () => {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-  return user;
-};
 
 export const getUserID = async () => {
-  const user = await getUser();
-  if (!user) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
     return;
   }
-  const userIDCode = String(user?.id);
-  const userID = user?.image ? `google_${userIDCode}` : `mongo_${userIDCode}`;
-  return userID;
+  return new mongoose.Types.ObjectId(session.user?.id);
+};
+
+const getUser = async () => {
+  try {
+    const id = await getUserID();
+    if (!id) {
+      console.error("Error: User ID is undefined or null.");
+      return null;
+    }
+
+    await connect();
+
+    const user = await User.findOne({ _id: id });
+    if (!user) {
+      console.warn(`Warning: No user found with ID ${id}`);
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return null;
+  }
 };
 
 export default getUser;
